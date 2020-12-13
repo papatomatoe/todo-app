acbc7de765dce22b736bfb19045ff4b84625bba4
papatomatoe <hole_in_the_wall@list.ru>
2020-12-13 17:24:28 +0300

    add: props

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,12 +3,13 @@ import React from 'react';
 import TaskList from '../TaskList/TaskList.js';
 import CreateTask from '../CreateTask/CreateTask.js';
 
-const Main = () => {
+const Main = (props) => {
+  const { tasks, addNewTask } = props;
   return (
     <main className="content container">
       <h1 className="visually-hidden">ToDo Application</h1>
-      <TaskList />
-      <CreateTask />
+      <TaskList tasks={tasks} />
+      <CreateTask addNewTask={addNewTask} />
     </main>
   );
 }
