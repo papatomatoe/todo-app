0d59a7fdc10e199dfeeb20ae14cec726e392808a
papatomatoe <hole_in_the_wall@list.ru>
2020-12-13 17:27:26 +0300

    add: prop done, class

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Task = (props) => {
-  const { title, isImportant } = props;
+  const { title, isImportant, done } = props;
   return (
-    <li className={`tasks__item ${isImportant && 'tasks__item--important'}`}>
+    <li className={`tasks__item ${isImportant && 'tasks__item--important'} ${done && 'tasks__item--done'}`}>
       <h3 className="tasks__title">{title}</h3>
       <div className="tasks__btns">
         <button className="task__btn task__btn--important" type="button" aria-label="make important"></button>
