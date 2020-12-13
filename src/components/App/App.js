8d02318b9c438802644ecc9fab0f940508cc37b1
papatomatoe <hole_in_the_wall@list.ru>
2020-12-13 17:21:02 +0300

    add: class-component, state, handler

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,14 +4,59 @@ import Header from '../Header/Header.js';
 import Main from '../Main/Main.js';
 import Footer from '../Footer/Footer.js';
 
-const App = () => {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Main />
-      <Footer currentYear={new Date().getFullYear()} />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    tasks: [
+      {
+        id: 1,
+        title: 'Drink Coffee',
+        isImportant: false,
+        done: false,
+      },
+      {
+        id: 2,
+        title: 'Read Book',
+        isImportant: false,
+        done: true,
+      }, {
+        id: 7,
+        title: 'Create React App',
+        isImportant: true,
+        done: false,
+      }
+    ]
+  }
+
+  addNewTaskHandler = (title, isImportant) => {
+    const tasks = this.state.tasks.slice();
+    const id = Math.max(...tasks.map(task => task.id)) + 1;
+
+    const newTask =
+    {
+      id,
+      title,
+      isImportant,
+      done: false,
+    };
+
+    tasks.push(newTask);
+
+    this.setState(({ tasks }));
+  }
+
+  render() {
+    const { tasks } = this.state;
+
+    console.log(tasks)
+
+    return (
+      <React.Fragment>
+        <Header />
+        <Main tasks={tasks} addNewTask={this.addNewTaskHandler} />
+        <Footer currentYear={new Date().getFullYear()} />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
